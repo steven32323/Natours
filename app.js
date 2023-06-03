457953ef8e2bb487824c5f8ee73e0395bba9663c
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-06-03 21:21:40 +0100

    implemented tour and user routes/controllers, refactored them into their own modules, created server module

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+const morgan = require('morgan');
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from the server side',
-    app: 'Natours',
-  });
-});
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint....');
-});
+// 1 - MIDDLEWARE
+app.use(morgan('dev'));
 
+app.use(express.json());
+
+// 3 - ROUTES
+
+app.use('/api/v1/tours', tourRouter);
+
+app.use('/api/v1/users', userRouter);
+
+// 4 -  START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
---
+++ controllers/tourController.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length || !tours) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  const tour = tours.find((e) => e.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+};
+
+exports.createTour = (req, res) => {
+  //   console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ status: 'failure', message: 'Server Error' });
+      }
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated Tour here>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  // 204 === no content i.e. data no longer exists(null)
+  res.status(204).json({
+    status: 'success',
+    data: {
+      tour: '<Deleted Tour>',
+    },
+  });
+};
